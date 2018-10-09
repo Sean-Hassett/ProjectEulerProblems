3183e6642487d5b67c65a60c5d87265fad7c217a
Sean-Hassett <hassetse@tcd.ie>
2018-10-09 20:31:01 +0100

    p004 in Go

---
+++ Go/p004/p004.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func isPalindrome(num int) bool {
+	s := strconv.Itoa(num)
+	n := len(s)
+	for n > 1 {
+		if s[0] != s[n-1] {
+			return false
+		}
+		s = s[1 : n-1]
+		n = len(s)
+	}
+	return true
+}
+
+func main() {
+	largestPalindrome := 0
+
+	for i := 999; i > 0; i-- {
+		for j := i; j > 0; j-- {
+			num := i * j
+			if isPalindrome(num) {
+				if num > largestPalindrome {
+					largestPalindrome = num
+				}
+			}
+		}
+	}
+	fmt.Println(largestPalindrome)
+}
